3f8e5ae8b5ef101e69dd3194c17335c4165f05ff
kenticoondrejp <ondrejp@kentico.com>
2017-06-20 09:26:52 +0200

    Initial commit

---
+++ CMS/Admin/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.UI;
+
+using CMS.Base;
+using CMS.Helpers;
+
+
+public partial class Admin_Default : Page
+{
+    protected override void OnPreInit(EventArgs e)
+    {
+        string customAdminPath = AdministrationUrlHelper.GetCustomAdministrationPath();
+        if (String.IsNullOrEmpty(customAdminPath) || customAdminPath.EqualsCSafe(AdministrationUrlHelper.DEFAULT_ADMINISTRATION_PATH, true))
+        {
+            URLHelper.Redirect("~/Admin/CMSAdministration.aspx" + RequestContext.URL.Query);
+        }
+        else
+        {
+            RequestHelper.Respond404();
+        }
+        base.OnPreInit(e);
+    }
+}
---
+++ CMS/CMSAdminControls/UI/Selectors/LoadGenerationSelector.ascx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+using CMS.Helpers;
+using CMS.UIControls;
+
+
+public partial class CMSAdminControls_UI_Selectors_LoadGenerationSelector : CMSUserControl
+{
+    private bool mNoChangeOption = false;
+
+
+    /// <summary>
+    /// If true, no change option (-1) is added to the list.
+    /// </summary>
+    public bool NoChangeOption
+    {
+        get
+        {
+            return mNoChangeOption;
+        }
+        set
+        {
+            mNoChangeOption = value;
+        }
+    }
+
+
+    /// <summary>
+    /// Selected value.
+    /// </summary>
+    public int Value
+    {
+        get
+        {
+            return ValidationHelper.GetInteger(drpGeneration.SelectedValue, 0);
+        }
+        set
+        {
+            try
+            {
+                drpGeneration.SelectedValue = value.ToString();
+            }
+            catch
+            {
+            }
+        }
+    }
+
+
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        if (NoChangeOption)
+        {
+            drpGeneration.Items.Add(new ListItem(GetString("LoadGeneration.NoChange"), "-1"));
+        }
+        drpGeneration.Items.Add(new ListItem(GetString("LoadGeneration.First"), "0"));
+        drpGeneration.Items.Add(new ListItem(GetString("LoadGeneration.Second"), "1"));
+        drpGeneration.Items.Add(new ListItem(GetString("LoadGeneration.Third"), "2"));
+    }
+}
